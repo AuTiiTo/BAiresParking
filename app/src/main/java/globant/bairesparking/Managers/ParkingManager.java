8f8695810916504d59f4ad757570d6bea9b13cab
s.ruiz <s.ruiz@globant.com>
2017-02-08 15:41:48 -0300

    Add:  - json file and managment  - generic database helper

--- app/src/main/java/globant/bairesparking/Managers/ParkingManager.java
+++ app/src/main/java/globant/bairesparking/Managers/ParkingManager.java
@@ -1,5 +1,13 @@
 package globant.bairesparking.Managers;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import globant.bairesparking.Commons.GsonConverter;
 import rx.Observable;
 
@@ -9,5 +17,12 @@ import rx.Observable;
 public class ParkingManager {
     public Observable<ParkingItem> requestParkings(String json) {
         return Observable.from(GsonConverter.getParkingList(json));
+    }
+
+    public static List<ParkingItem> getParkingList(Reader jsonRaw) {
+        Gson gson = new Gson();
+        Type typeOfT = new TypeToken<ArrayList<ParkingItem>>(){}.getType();
+        List<ParkingItem> response = (List<ParkingItem>) gson.fromJson(jsonRaw, typeOfT);
+        return response;
     }
 }
